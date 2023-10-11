6290f8ea5af296ffcb51f1d46cf5127a7c972752
Manoj_Mirashi <manojmirashi2018@gmail.com>
2023-10-11 21:15:00 +0530

    Add files via upload
    to find a gretest  of 2 numbers from the given integer

---
+++ space.java
@@ -0,0 +1,11 @@
+class space 
+{
+	public static void main(String[] args) 
+	{
+		int a=12;
+		int b=13;
+//to finding a gretest  of 2 numbers from the given integer
+		int d=a>b?a:b;//this is the logic of conditional operator...
+		System.out.println(d);
+	}
+}
